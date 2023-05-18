8a3e1ba84d28bcad4ceac3c5872307fd20d141c2
mesciuS <giovadev@gmail.com>
2023-05-18 20:07:11 +0200

    feat: destroy

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -9,5 +9,10 @@
             <li>Artisti: {{$comic->artists}}</li>
             <li>Scrittori: {{$comic->writers}}</li>
         </ul>
+        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" style="color: red">CANCELLA!</button>
+        </form>
     </div>
 @endsection
